5ae9288bcae76adf715039ca5b502190017d2a52
Carlos <nzlosh@yahoo.com>
2019-01-18 07:56:19 +0100

    add setup.py for pip install support.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    author_email="nobody@localhost",
+    author="err-backend-gitter contributors",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    description="A Gitter.im backend for Errbot.",
+    keywords='errbot gitter.im',
+    long_description_content_type="text/markdown",
+    long_description="Not available",
+    name="err-backend-gitter.im",
+    packages=setuptools.find_packages(),
+    url="https://github.com/nzlosh/err-backend-gitter",
+    version="0.0.1",
+)
